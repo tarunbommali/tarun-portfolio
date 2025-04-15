98a88d04a0ddb08910e772f0715fa1e855840ecb
Tarun <btaruntej143@gamail.com>
2025-04-15 13:28:30 +0530

    bug fixes

--- src/components/About/ServicesList.jsx
+++ src/components/About/ServicesList.jsx
@@ -1,5 +1,44 @@
 import { BentoGrid, BentoGridItem } from "../ui/bento-grid";
-import { servicesList } from "../../utils/servicesList";
+
+const servicesList = [
+  {
+      id: 0,
+      imgUrl: "https://res.cloudinary.com/drdgj0pch/image/upload/v1711474687/icons8-web-design-48_gnapn7.png",
+      displayText: "RESPONSIVE DESIGN",
+      description: "Designs that adapt smoothly to all screen sizes and devices."
+  },
+  {
+      id: 1,
+      imgUrl: "https://img.icons8.com/fluency/48/cloud-sync--v1.png",
+      displayText: "LIGHT SPEED",
+      description: "Optimized for fast loading and seamless performance."
+  },
+  {
+      id: 2,
+      imgUrl: "https://img.icons8.com/fluency/48/security-user-male.png",
+      displayText: "GOOGLE AUTHENTICATION",
+      description: "Secure login using Google account integration."
+  },
+  {
+      id: 3,
+      imgUrl: "https://res.cloudinary.com/drdgj0pch/image/upload/v1711474591/icons8-chat-48_nhdaih.png",
+      displayText: "LIVE CHAT",
+      description: "Real-time messaging support for instant communication."
+  },
+  {
+      id: 4,
+      imgUrl: "https://res.cloudinary.com/drdgj0pch/image/upload/v1711474585/icons8-api-48_h8s1b5.png",
+      displayText: "API Integration",
+      description: "Seamless integration with third-party APIs and services."
+  },
+  {
+      id: 5,
+      imgUrl: "https://res.cloudinary.com/drdgj0pch/image/upload/v1711474581/icons8-testing-48_pv9ahs.png",
+      displayText: "Testing",
+      description: "Ensuring reliability with unit, integration, and UI testing."
+  }
+];
+
 
 export function ServicesList() {
   return (
